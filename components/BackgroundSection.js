468d4acd4f12157bcdf6e0066cf35f461a1347ee
cmooredev <cmooredev@pm.me>
2023-04-06 16:01:29 -0700

    update readme

--- components/BackgroundSection.js
+++ components/BackgroundSection.js
@@ -3,28 +3,31 @@ import { Element } from 'react-scroll';
 import { motion } from 'framer-motion';
 
 const BackgroundSection = ({ id, imgSrc, children }) => {
-  const style = {
-    backgroundImage: `url(${imgSrc})`,
-    backgroundSize: 'cover',
-    backgroundPosition: 'center',
-    minHeight: '100vh',
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
+    const style = {
+      backgroundImage: `url(${imgSrc})`,
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+      backgroundRepeat: 'no-repeat', // Prevent image repetition
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
+  
+    return (
+      <Element name={id}>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          style={style}
+          className="background-section-container" // Apply the class here
+        >
+          {children}
+        </motion.div>
+      </Element>
+    );
   };
-
-  return (
-    <Element name={id}>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 0.8 }}
-        style={style}
-      >
-        {children}
-      </motion.div>
-    </Element>
-  );
-};
+  
 
 export default BackgroundSection;
